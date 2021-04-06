d2f6c5a53021daa6fa97e56e9906f898ed5af7cc
Riya Rana <75501058+ria-debug@users.noreply.github.com>
2021-04-06 12:53:53 -0400

    Update Token function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,12 +1,18 @@
-pragma solidity ^0.5.16;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
+
     constructor(uint256 initialSupply) public ERC20("RIYA", "RIA") {
-     _mint(msg.sender);
+    _mint(msg.sender, initialSupply);
         
     }
- 
+    
+    function generateTokens(address _beneficiary, uint256 _tokens) public {
+        
+        transfer(_beneficiary, _tokens);
+        
+    }
 }
 
